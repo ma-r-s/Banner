a56fa20f234e9cfd0e318ad3d3d51d9a61f8587d
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-09-09 14:57:10 -0500

    FC

---
+++ src/lib/Toggle.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { toggleMode } from 'mode-watcher';
+	import { SunMedium, Moon } from 'lucide-svelte';
+</script>
+
+<Button on:click={toggleMode} variant="ghost" size="icon">
+	<SunMedium class="size-4 dark:hidden" />
+	<Moon class="hidden size-4 dark:block" />
+	<span class="sr-only">Toggle theme</span>
+</Button>
